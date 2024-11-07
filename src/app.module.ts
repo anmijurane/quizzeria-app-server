5e37d3c3aa50f7cacfe83169eb0124b7dc6af329
M. Andres Jurado <anmijurane@gmail.com>
2024-11-07 00:29:51 -0600

    [ADD]: import module Subject and Questions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PostgresTypeORMModule } from './modules.exports';
+import { SubjectsModule } from './subjects/subjects.module';
+import { QuestionsModule } from './questions/questions.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PostgresTypeORMModule()],
+  imports: [
+    ConfigModule.forRoot(),
+    PostgresTypeORMModule(),
+    SubjectsModule,
+    QuestionsModule,
+  ],
   controllers: [],
   providers: [],
 })
