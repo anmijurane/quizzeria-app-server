825814fee08d2520a9c293c164dfb0b43b2a4302
M. Andres Jurado <anmijurane@gmail.com>
2024-11-06 23:51:29 -0600

    [ADD]: Prostres configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PostgresTypeORMModule } from './modules.exports';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), PostgresTypeORMModule()],
   controllers: [],
   providers: [],
 })
